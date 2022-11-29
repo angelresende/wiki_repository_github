90c8dbdc8f1497a0868e280a7a6f85ead853db57
Angelica Resende Araujo <angelica.resende@ufenesp.com.br>
2022-11-29 09:24:55 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from './pages/App'
+
+import GlobalStyles from './styles/styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 )
